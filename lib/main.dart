8c5d10eb5136c73b7dc305a0a01141a7adc100fc
thiteewongmun <111643727+thiteewongmun@users.noreply.github.com>
2022-11-23 04:42:12 +0700

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,60 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:wee001/home_screen.dart';
 
 void main() {
-  runApp(const MyWidget());
+  runApp(const MyApp());
 }
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "app",
-      home: MyHomepage(),
-      theme: ThemeData(primarySwatch: Colors.red),
+    return const MaterialApp(
+      title: 'INFINITAS',
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
     );
-  }
-}
-//สร้าง -------
-
-class MyHomepage extends StatefulWidget {
-  const MyHomepage({super.key});
-
-  @override
-  State<MyHomepage> createState() => _MyHomepageState();
-}
-
-int number = 0; //การสร้าง state
-
-@override
-class _MyHomepageState extends State<MyHomepage> {
-  Widget build(BuildContext context) {
-    //กลุ่มข้อมูล text wiget
-    List<Widget> data = [];
-    for (var i = 0; i < 10; i++) {
-      data.add(Text("รายการ${i + 1}"));
-    }
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("โปรแกรมนับตัวเลข"),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: data,
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: addnumber,
-        child: Text("เพิ่ม"),
-      ),
-    );
-  }
-
-  void addnumber() {
-    setState(() {
-      number++;
-    });
   }
 }
